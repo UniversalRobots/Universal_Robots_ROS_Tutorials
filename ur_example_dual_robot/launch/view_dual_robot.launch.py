faac18e26b34fbf3e7087d157fcfa128fd688289
Felix Exner <exner@fzi.de>
2022-02-17 09:48:58 +0100

    ROS2 dual robot description

---
+++ ur_example_dual_robot/launch/view_dual_robot.launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("ur_example_dual_robot"), "urdf", "dual_robot.xacro"]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("ur_example_dual_robot"), "etc", "view_robot.rviz"]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui", executable="joint_state_publisher_gui"
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    nodes_to_start = [joint_state_publisher_node, robot_state_publisher_node, rviz_node]
+
+    return LaunchDescription(nodes_to_start)
